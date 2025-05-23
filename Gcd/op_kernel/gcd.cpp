1c074e5f394e7ddeee31b426a2abbd07d942aac9
sunmingzhi <531483935@qq.com>
2025-05-23 16:44:12 +0800

    Update

--- Gcd/op_kernel/gcd.cpp
+++ Gcd/op_kernel/gcd.cpp
@@ -1,5 +1,7 @@
 #include "kernel_operator.h"
 using namespace AscendC;
+constexpr int32_t BUFFER_NUM = 2;
+constexpr int32_t tile_length = 512;
 template<typename T> class BruteForce {
 public:
     __aicore__ inline BruteForce() {}
@@ -61,24 +63,55 @@ private:
 template<typename T> class GCDKernalFast {
     public:
         __aicore__ inline GCDKernalFast() {}
-        __aicore__ inline void Init(GM_ADDR x1, GM_ADDR x2, GM_ADDR y, uint32_t size, uint32_t length) {
+        __aicore__ inline void Init(GM_ADDR x1, GM_ADDR x2, GM_ADDR y, uint32_t size, uint32_t total) {
             ASSERT(GetBlockNum() != 0 && "block dim can not be zero!");
             const unsigned num_cores = GetBlockNum();
-            unsigned L = GetBlockIdx() * length;
-            unsigned R = (GetBlockIdx() + 1) * length;
+            unsigned L = GetBlockIdx() * total;
+            unsigned R = (GetBlockIdx() + 1) * total;
             if (R > size) {
                 R = size;
             }
             this->L = L;
             this->R = R;
-            x1Gm.SetGlobalBuffer((__gm__ T*)x1, length + num_cores);
-            x2Gm.SetGlobalBuffer((__gm__ T*)x2, length + num_cores);
-            yGm.SetGlobalBuffer((__gm__ T*)y, length + num_cores);
+            x1Gm.SetGlobalBuffer((__gm__ T*)x1, total);
+            x2Gm.SetGlobalBuffer((__gm__ T*)x2, total);
+            yGm.SetGlobalBuffer((__gm__ T*)y, total);
+            pipe.InitBuffer(Q_x1, BUFFER_NUM, tile_length * sizeof(T));
+            pipe.InitBuffer(Q_x2, BUFFER_NUM, tile_length * sizeof(T));
+            pipe.InitBuffer(Q_y, BUFFER_NUM, tile_length * sizeof(T));
+            pipe.InitBuffer(B_tmp, tile_length * sizeof(T));
+            pipe.InitBuffer(B_zero, tile_length * sizeof(T));
+            pipe.InitBuffer(B_bits, tile_length * sizeof(uint8_t));
+            zero = B_zero.Get<T>();
+            Duplicate(zero, T(0), tile_length);
+        }
+        __aicore__ inline void CopyIn(int32_t pos) {
+            LocalTensor<T> x1 = Q_x1.AllocTensor<T>();
+            LocalTensor<T> x2 = Q_x2.AllocTensor<T>();
+            DataCopy(x1, x1Gm[pos], tile_length);
+            DataCopy(x2, x2Gm[pos], tile_length);
+            Q_x1.EnQue(x1);
+            Q_x2.EnQue(x2);
+        }
+        __aicore__ inline void CopyOut(int32_t pos) {
+            LocalTensor<T> y = Q_y.DeQue<T>();
+            DataCopy(yGm[pos], y, tile_length);
+            Q_y.FreeTensor(y);
         }
         __aicore__ inline void Process() {
-            for (int i = L; i < R; ++i) {
-                T a = x1Gm.GetValue(i);
-                T b = x2Gm.GetValue(i);
+            for (int i = L; i < R; i += tile_length) {
+                CopyIn(i);
+                Compute(i);
+                CopyOut(i);
+            }
+        }
+        __aicore__ inline void Compute(int32_t pos) {
+            LocalTensor<T> a = Q_x1.DeQue<T>();
+            LocalTensor<T> b = Q_x2.DeQue<T>();
+            LocalTensor<T> y = Q_y.AllocTensor<T>();
+            for (int i = 0; i < tile_length; ++i) {
+                T a = x1.GetValue(i);
+                T b = x2.GetValue(i);
                 while (b) {
                     // T A = b;
                     // T B = a % b;
@@ -87,14 +120,29 @@ template<typename T> class GCDKernalFast {
                     a = a % b;
                     a ^= b ^= a ^= b;
                 }
-                yGm.SetValue(i, a > 0 ? a : -a);
+                y.SetValue(i, a > 0 ? a : -a);
             }
-            //AscendC::DataCacheCleanAndInvalid<T, AscendC::CacheLine::ENTIRE_DATA_CACHE, AscendC::DcciDst::CACHELINE_OUT>(yGm);
-            //DataCacheCleanAndInvalid<T, CacheLine::ENTIRE_DATA_CACHE>(yGm);
+            // auto tmp = B_tmp.Get<T>();
+            // auto bits = B_bits.Get<uint8_t>();
+            // for (int i = 0; i < 64; ++i) {
+            //     Div(tmp, a, b, tile_length);
+            //     Mul(tmp, tmp, b, tile_length);
+            //     Sub(tmp, a, tmp, tile_length);
+
+            //     Compare(bits, tmp, zero, CMPMODE::GT, length);
+            // }
+
+            Q_x1.FreeTensor(a);
+            Q_x2.FreeTensor(b);
+            Q_y.EnQue<T>(y);
         }
-    
     private:
+        TPipe pipe;
+        TBuf<QuePosition::VECCALC> B_bits, B_tmp, B_zero;
+        TQue<QuePosition::VECIN, BUFFER_NUM> Q_x1, Q_x2;
+        TQue<QuePosition::VECOUT, BUFFER_NUM> Q_y;
         GlobalTensor<T> x1Gm, x2Gm, yGm;
+        LocalTensor<T> zero;
         uint32_t L, R;
 };
 extern "C" __global__ __aicore__ void gcd(GM_ADDR x1, GM_ADDR x2, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
