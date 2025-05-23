c78ddcffc207c7d558b295f0a23995f743b02d59
sunmingzhi <531483935@qq.com>
2025-05-23 16:47:00 +0800

    Update

--- Gcd/op_kernel/gcd.cpp
+++ Gcd/op_kernel/gcd.cpp
@@ -80,10 +80,10 @@ template<typename T> class GCDKernalFast {
             pipe.InitBuffer(Q_x2, BUFFER_NUM, tile_length * sizeof(T));
             pipe.InitBuffer(Q_y, BUFFER_NUM, tile_length * sizeof(T));
             pipe.InitBuffer(B_tmp, tile_length * sizeof(T));
-            pipe.InitBuffer(B_zero, tile_length * sizeof(T));
+            //pipe.InitBuffer(B_zero, tile_length * sizeof(T));
             pipe.InitBuffer(B_bits, tile_length * sizeof(uint8_t));
-            zero = B_zero.Get<T>();
-            Duplicate(zero, T(0), tile_length);
+            //zero = B_zero.Get<T>();
+            //Duplicate(zero, T(0), tile_length);
         }
         __aicore__ inline void CopyIn(int32_t pos) {
             LocalTensor<T> x1 = Q_x1.AllocTensor<T>();
@@ -132,17 +132,17 @@ template<typename T> class GCDKernalFast {
             //     Compare(bits, tmp, zero, CMPMODE::GT, length);
             // }
 
-            Q_x1.FreeTensor(a);
-            Q_x2.FreeTensor(b);
+            Q_x1.FreeTensor(x1);
+            Q_x2.FreeTensor(x2);
             Q_y.EnQue<T>(y);
         }
     private:
         TPipe pipe;
-        TBuf<QuePosition::VECCALC> B_bits, B_tmp, B_zero;
+        TBuf<QuePosition::VECCALC> B_bits, B_tmp;//, B_zero;
         TQue<QuePosition::VECIN, BUFFER_NUM> Q_x1, Q_x2;
         TQue<QuePosition::VECOUT, BUFFER_NUM> Q_y;
         GlobalTensor<T> x1Gm, x2Gm, yGm;
-        LocalTensor<T> zero;
+        //LocalTensor<T> zero;
         uint32_t L, R;
 };
 extern "C" __global__ __aicore__ void gcd(GM_ADDR x1, GM_ADDR x2, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
